0566e6ded13d95a28c2c3a7ba72e05688821b844
ezmcgse <sean.s.mcgrath@ericsson.com>
2018-04-27 09:12:50 +0100

    created a notfound route page.

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var React = require('react');
-var Link = require('react-router').Link
+var Link = require('react-router').Link;
 
 var Home = React.createClass({
 	render: function() {
@@ -9,7 +9,7 @@ var Home = React.createClass({
 			<div className="jumbotron">
 				<h1>Pluralsight Administration</h1>
 				<p>React, React Router, and Flux for ultra-responsive web apps.</p>
-				<Link to="about" className="btn btn-primary btn-large"></Link>
+				<Link to="about" className="btn btn-primary btn-large">Go To About</Link>
 			</div>
 		);
 	}
